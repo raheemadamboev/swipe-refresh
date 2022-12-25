ca55feb0248c1b4985076db075b3bcbc061bc1aa
raheemadamboev <vividricemaryandmax@gmail.com>
2022-12-25 22:28:37 +0500

    implement swipe refresh

--- app/src/main/java/xyz/teamgravity/swiperefresh/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/swiperefresh/MainActivity.kt
@@ -4,9 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import xyz.teamgravity.swiperefresh.ui.theme.SwipeRefreshTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,6 +28,31 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    var loading by remember { mutableStateOf(false) }
+
+                    val scope = rememberCoroutineScope()
+                    val refreshState = rememberSwipeRefreshState(isRefreshing = loading)
+
+                    SwipeRefresh(
+                        state = refreshState,
+                        onRefresh = {
+                            scope.launch {
+                                loading = true
+                                delay(3_000L)
+                                loading = false
+                            }
+                        },
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
+                            items(100) { index ->
+                                Text(
+                                    text = stringResource(id = R.string.your_item_index, index),
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
